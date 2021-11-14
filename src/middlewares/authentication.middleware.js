e3ba61c2840ef6fa758cf9cb66c838a701a8de3c
tomisargiottob <tomisargiottob@hotmail.com>
2021-11-14 13:19:24 +0000

    feature(auth): add authenification middleware

---
+++ src/middlewares/authentication.middleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const config = require('config');
+
+function verifyToken(req, res, next) {
+  if (req.url !== '/login' && (req.url !== '/user' && req.method !== 'POST')) {
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    if (!token) {
+      return res.status(401).send('Unauthorized');
+    }
+    try {
+      const decoded = jwt.verify(token, config.auth.secret);
+      req.user = decoded;
+    } catch (err) {
+      return res.status(401).send('Unauthorized');
+    }
+    return next();
+  }
+  return next();
+}
+module.exports = { verifyToken };
