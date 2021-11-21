0b9f2f3e039eb0be2016bf710763ec8852ceeb70
tomisargiottob <tomisargiottob@hotmail.com>
2021-11-21 12:27:03 +0000

    refactor(collection/user): remove model user from collection

--- src/components/database/collections/users.js
+++ src/components/database/collections/users.js
@@ -7,10 +7,10 @@ class Users {
     this.logger = logger;
   }
 
-  async getAllUsers() {
+  async getAllUsers({ where }) {
     try {
-      const usersFetched = await this.collection.find();
-      const users = usersFetched.map((user) => new UserModel(user));
+      const usersFetched = await this.collection.find(where);
+      const users = usersFetched.map((user) => new UserModel(this.collection, user));
       return users;
     } catch (err) {
       this.logger.error(err);
@@ -18,11 +18,11 @@ class Users {
     }
   }
 
-  async getUser({ username }) {
+  async getUser(username) {
     try {
       const user = await this.collection.findOne({ username });
       if (user) {
-        return new UserModel(user);
+        return new UserModel(this.collection, user);
       }
       return user;
     } catch (err) {
@@ -31,11 +31,11 @@ class Users {
     }
   }
 
-  async getUserById({ id }) {
+  async getUserById(id) {
     try {
       const user = await this.collection.findOne({ _id: id });
       if (user) {
-        return new UserModel(user);
+        return new UserModel(this.collection, user);
       }
       return user;
     } catch (err) {
@@ -47,60 +47,13 @@ class Users {
   async createUser(data) {
     try {
       const user = data;
-      await this.collection.insertOne(data);
-      return new UserModel(user);
+      user.active = true;
+      user.apiKey = [];
+      await this.collection.insertOne(user);
+      return new UserModel(this.collection, user);
     } catch (err) {
       this.logger.error(err);
       return err;
-    }
-  }
-
-  async updateUser(id, updateData) {
-    let {
-      password,
-      apiKey,
-      schedule,
-      wallet,
-      active,
-    } = updateData;
-    try {
-      const user = await this.getUserById({ id });
-      if (!user) {
-        return new Error({ message: 'User does not exist' });
-      }
-      if (!apiKey) {
-        apiKey = user.apiKey;
-      }
-      if (!password) {
-        password = user.password;
-      }
-      if (!schedule) {
-        schedule = user.schedule;
-      }
-      if (!wallet) {
-        wallet = user.wallet;
-      }
-      if (!active) {
-        active = user.active;
-      }
-      const userUpdated = await this.collection.findOneAndUpdate(
-        { _id: id },
-        {
-          $set:
-            {
-              apiKey,
-              password,
-              schedule,
-              wallet,
-              active,
-            },
-        },
-        { returnDocument: 'after' },
-      );
-      return new UserModel(userUpdated.value);
-    } catch (err) {
-      this.logger.error(err);
-      return '';
     }
   }
 }
