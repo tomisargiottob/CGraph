c20178f2527fd04520ff5f1abd52606d7715be2c
tomisargiottob <tomisargiottob@hotmail.com>
2021-11-21 12:25:27 +0000

    feature(controller/wallet): create wallet controller

--- src/api/controllers/user/{id}/wallet.js
+++ src/api/controllers/user/{id}/wallet.js
@@ -1,12 +1,22 @@
-module.exports = function userWalletCollection(db, logger) {
+module.exports = function userWalletController(db, logger) {
   return {
     get: async function registerUser(req, res) {
-      console.log('llega');
-      const { username, password } = req.body;
-      console.log(username, password);
-      const user = db.user.getUser(username);
-      console.log(user);
-      res.status(200).json(user);
+      const { id } = req.params;
+      const { where } = req.query;
+      const filter = {};
+      if (where) {
+        filter.where = where;
+      } else {
+        const today = new Date();
+        const pastDate = today.setMonth(today.getMonth() - 3);
+        filter.where = {
+          gt: pastDate.getTime(),
+          lt: today.getTime(),
+        };
+      }
+      logger.info(`Getting wallet between dates ${filter.where.gt} and ${filter.where.lt}`);
+      const wallet = await db.wallet.getUserWallet(id, filter);
+      res.status(200).json(wallet);
     },
   };
 };
